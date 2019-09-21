f64d4bb82ea38a3cd63bf46bc8fb294e90987159
paruobro <paruobro@gmail.com>
2019-09-21 13:31:03 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Maitoparta on 2019/09/21.
+//  Copyright © 2019 paruobro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
